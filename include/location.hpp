bdaa441f39ee2584bdf4a4c855478264bc5aa53f
Mike Sargsyan <145457464+Wolframike@users.noreply.github.com>
2024-08-18 17:08:27 +0900

    Migrated relevant files from the previous version Added first version of Result<T> class

---
+++ include/location.hpp
@@ -0,0 +1,39 @@
+#ifndef LOCATION_HPP_
+#define LOCATION_HPP_
+
+#include "typedefs.hpp"
+
+class Location {
+private:
+	// parsed
+	short methods_;
+	std::string root_;
+	bool autoindex_;
+	std::string index_;
+	std::vector<std::string> extensions_;
+	std::string upload_path_;
+	std::string redirect_;
+
+public:
+	// setter, getter
+	Location();
+	void addMethod(const HttpMethod);
+	void setRoot(const std::string&);
+	void setAutoindex(bool);
+	void setIndex(const std::string&);
+	void addExtension(const std::string&);
+	void setUploadPath(const std::string&);
+	void setRedirect(const std::string&);
+
+	short getMethods() const;
+	const std::string& getRoot() const;
+	bool isAutoIndex() const;
+	const std::string& getIndex() const;
+	const std::vector<std::string>& getExtensions() const;
+	const std::string& getUploadPath() const;
+	const std::string& getRedirect() const;
+	// debug
+	void print();
+};
+
+#endif    // LOCATION_HPP_
---
+++ src/config/server_config.cpp
@@ -0,0 +1,41 @@
+#include "config.hpp"
+
+ServerConfig::ServerConfig(): host_(""), port_(""), server_name_(""), max_body_size_(MAX_BODY_SIZE) {}
+
+void ServerConfig::setHost(const std::string& host) { this->host_ = host; }
+
+void ServerConfig::setPort(const std::string& port) { this->port_ = port; }
+
+void ServerConfig::setServerName(const std::string& server_name) {
+	this->server_name_ = server_name;
+}
+
+void ServerConfig::setMaxBodySize(int max_body_size) {
+	this->max_body_size_ = max_body_size;
+}
+
+void ServerConfig::addLocation(const Location& location) {
+	this->locations_.push_back(location);
+}
+
+void ServerConfig::addError(int status, const std::string& path) {
+	this->errors_.insert(std::pair<HttpStatus, std::string>(static_cast<HttpStatus>(status), path));
+}
+
+const std::map<HttpStatus, std::string>& ServerConfig::getErrors() const {
+	return this->errors_;
+}
+
+const std::vector<Location>& ServerConfig::getLocations() const {
+	return this->locations_;
+}
+
+const std::string& ServerConfig::getHost() const { return this->host_; }
+
+const std::string& ServerConfig::getPort() const { return this->port_; }
+
+const std::string& ServerConfig::getServerName() const {
+	return this->server_name_;
+}
+
+int ServerConfig::getMaxBodySize() const { return this->max_body_size_; }
