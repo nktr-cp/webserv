36d7590a239e921e8746933c2b23eb8007247c6b
Mike Sargsyan <145457464+Wolframike@users.noreply.github.com>
2024-08-28 14:15:56 +0900

    [fix] config minor tweak - Location::setIndex no longer exists, with it replaced with Location::addIndex. - Added Location::setName, Location::getName to set & get entry after keyword location in configuration file. - Index page is now bad for your eyes

--- include/location.hpp
+++ include/location.hpp
@@ -7,9 +7,10 @@ class Location {
  private:
   // parsed
   short methods_;
+  std::string name_;
   std::string root_;
   bool autoindex_;
-  std::string index_;
+  std::vector<std::string> index_;
   std::vector<std::string> extensions_;
   std::string upload_path_;
   std::string redirect_;
@@ -18,17 +19,19 @@ class Location {
   // setter, getter
   Location();
   void addMethod(const HttpMethod);
+  void setName(const std::string&);
   void setRoot(const std::string&);
   void setAutoindex(bool);
-  void setIndex(const std::string&);
+  void addIndex(const std::string&);
   void addExtension(const std::string&);
   void setUploadPath(const std::string&);
   void setRedirect(const std::string&);
 
   short getMethods() const;
+  const std::string& getName() const;
   const std::string& getRoot() const;
   bool isAutoIndex() const;
-  const std::string& getIndex() const;
+  const std::vector<std::string>& getIndex() const;
   const std::vector<std::string>& getExtensions() const;
   const std::string& getUploadPath() const;
   const std::string& getRedirect() const;
--- src/location.cpp
+++ src/location.cpp
@@ -4,7 +4,7 @@ Location::Location()
     : methods_(0),
       root_(""),
       autoindex_(false),
-      index_(""),
+      index_(),
       extensions_(),
       upload_path_(""),
       redirect_("") {}
@@ -13,11 +13,13 @@ void Location::addMethod(const HttpMethod method) {
   this->methods_ |= static_cast<short>(method);
 }
 
+void Location::setName(const std::string& name) { this->name_ = name; }
+
 void Location::setRoot(const std::string& root) { this->root_ = root; }
 
 void Location::setAutoindex(bool autoindex) { this->autoindex_ = autoindex; }
 
-void Location::setIndex(const std::string& index) { this->index_ = index; }
+void Location::addIndex(const std::string& index) { this->index_.push_back(index); }
 
 void Location::addExtension(const std::string& extension) {
   this->extensions_.push_back(extension);
@@ -33,11 +35,13 @@ void Location::setRedirect(const std::string& redirect) {
 
 short Location::getMethods() const { return this->methods_; }
 
+const std::string& Location::getName() const { return this->name_; }
+
 const std::string& Location::getRoot() const { return this->root_; }
 
 bool Location::isAutoIndex() const { return this->autoindex_; }
 
-const std::string& Location::getIndex() const { return this->index_; }
+const std::vector<std::string>& Location::getIndex() const { return this->index_; }
 
 const std::vector<std::string>& Location::getExtensions() const {
   return this->extensions_;
@@ -50,10 +54,15 @@ const std::string& Location::getUploadPath() const {
 const std::string& Location::getRedirect() const { return this->redirect_; }
 
 void Location::print() {
+  std::cout << "Name: " << this->name_ << std::endl;
   std::cout << "Methods: " << this->methods_ << std::endl;
   std::cout << "Root: " << this->root_ << std::endl;
   std::cout << "Autoindex: " << this->autoindex_ << std::endl;
-  std::cout << "Index: " << this->index_ << std::endl;
+  std::cout << "Index: ";
+  for (size_t i = 0; i < this->index_.size(); i++) {
+    std::cout << this->index_[i] << " ";
+  }
+  std::cout << std::endl;
   std::cout << "Extensions: ";
   for (size_t i = 0; i < this->extensions_.size(); i++) {
     std::cout << this->extensions_[i] << " ";
