d2684c010175c865ef166402adc49216f02ac74b
oda251 <pgwyaueu@gmail.com>
2024-09-17 03:05:21 +0900

    [fixed]adapted satic-tester to current branch

--- test_static.py
+++ test_static.py
@@ -9,28 +9,28 @@ def test_static():
 	try:
 		print("--- test correct static requeset with [GET POST DELETE] ---")
 		data = "testdata"
-		print("[1]sending POST request to http://localhost:8080/html/test, expecting 200")
-		response = requests.post("http://localhost:8080/html/test", data=data)
+		print("[1]sending POST request to http://localhost:8080/test/test, expecting 200")
+		response = requests.post("http://localhost:8080/test/test", data=data)
 		assert response.status_code == 200, f"[1] Failed to get 200 response. {response.status_code}"
-		print("[2]sending GET request to http://localhost:8080/html/test, expecting 200")
-		response = requests.get("http://localhost:8080/html/test")
+		print("[2]sending GET request to http://localhost:8080/test/test, expecting 200")
+		response = requests.get("http://localhost:8080/test/test")
 		assert response.status_code == 200, f"[2] Failed to get response from server. {response.status_code}"
 		print("[3]checking content of response")
 		assert response.text == data, f"[3] Failed to get expected content. {data}"
-		print("[4]sending DELETE request to http://localhost:8080/html/test, expecting 200")
-		response = requests.delete("http://localhost:8080/html/test")
+		print("[4]sending DELETE request to http://localhost:8080/test/test, expecting 200")
+		response = requests.delete("http://localhost:8080/test/test")
 		assert response.status_code == 200, f"[4] Failed to get 200 response. {response.status_code}"
-		print("[5]sending GET request to http://localhost:8080/html/test, expecting 404")
-		response = requests.get("http://localhost:8080/html/test")
+		print("[5]sending GET request to http://localhost:8080/test/test, expecting 404")
+		response = requests.get("http://localhost:8080/test/test")
 		assert response.status_code == 404, f"[5] Failed to get 404 response from server. {response.status_code}"
 
 		print("--- test incorrect static requeset with [GET POST DELETE] ---")
-		print("[6]sending too large POST request to http://localhost:8080/html/test, expecting 413")
+		print("[6]sending too large POST request to http://localhost:8080/test, expecting 413")
 		data = "a" * 10000
-		response = requests.post("http://localhost:8080/html/test", data=data)
+		response = requests.post("http://localhost:8080/test", data=data)
 		assert response.status_code == 413, f"[6] Failed to get 413 response. {response.status_code}"
-		print("[7]sending GET request with too long uri to http://localhost:8080/html/test, expecting 414")
-		response = requests.get("http://localhost:8080/html/test" + "/a" * 10000)
+		print("[7]sending GET request with too long uri to http://localhost:8080/test, expecting 414")
+		response = requests.get("http://localhost:8080/test" + "/a" * 10000)
 		assert response.status_code == 414, f"[7] Failed to get 414 response. {response.status_code}"
 
 	finally:
