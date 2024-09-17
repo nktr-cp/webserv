04885423129b09c46d44b4ba3dade82d188231ef
oda251 <pgwyaueu@gmail.com>
2024-09-17 18:54:42 +0900

    [fixed]fixed test_static.py. previous version accesses wrong url in test 6

--- test_static.py
+++ test_static.py
@@ -25,9 +25,9 @@ def test_static():
 		assert response.status_code == 404, f"[5] Failed to get 404 response from server. {response.status_code}"
 
 		print("--- test incorrect static requeset with [GET POST DELETE] ---")
-		print("[6]sending too large POST request to http://localhost:8080/test, expecting 413")
+		print("[6]sending too large POST request to http://localhost:8080/test/test, expecting 413")
 		data = "a" * 10000
-		response = requests.post("http://localhost:8080/test", data=data)
+		response = requests.post("http://localhost:8080/test/test", data=data)
 		assert response.status_code == 413, f"[6] Failed to get 413 response. {response.status_code}"
 		print("[7]sending GET request with too long uri to http://localhost:8080/test, expecting 414")
 		response = requests.get("http://localhost:8080/test" + "/a" * 10000)
